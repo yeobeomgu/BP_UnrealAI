cdfea86a473ed0b70bc2088af3721b10379b8ee6
yeobeomgu <beomgu1029@gmail.com>
2023-08-22 17:43:31 +0900

    Unreal AI
    랜덤 이동
    시야 반응, 사운드 반응 기초 구현

---
+++ UnrealAIBlue/SpaceAI/Source/SpaceAI/SpaceAIGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SpaceAIGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASpaceAIGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASpaceAIGameMode();
+};
+
+
+
